ff7d65685b3f8219d85b53f06deaf86459835b63
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-09-04 22:04:50 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const button = document.querySelector(".btn");
+  const description = document.querySelector("#new-task-description");
+  const div = document.querySelector("#tasks");
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const list = document.createElement("li");
+    list.textContent = description.value;
+    div.appendChild(list);
+  });
 });
